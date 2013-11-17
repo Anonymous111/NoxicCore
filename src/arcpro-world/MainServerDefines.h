d82500333ec9cb68857b7ca4a9c0aab2b022f5dd
Zark <zdroid9770@yahoo.com>
2013-11-16 22:13:00 -0600

    Update MainServerDefines.h

--- src/arcpro-world/MainServerDefines.h
+++ src/arcpro-world/MainServerDefines.h
@@ -2,7 +2,7 @@
  * ArcPro MMORPG Server
  * Copyright (c) 2011-2013 ArcPro Speculation <http://www.arcpro.info/>
  * Copyright (c) 2008-2013 ArcEmu Team <http://www.arcemu.org/>
- * Copyright (c) 2008 OpenAscent
+ * Copyright (c) 2007-2008 OpenAscent
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
