e904b5ae237edff32f2f406cdac96b381a616630
zdroid9770 <zdroid9770@yahoo.com>
2013-11-24 02:43:02 -0600

    Removed deprecated SQLs

--- sql/world_updates/2013-10-28_07-54_warsong_gulch.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM `worldstate_templates` WHERE `map`='489' AND `zone`='3277' AND `field`='4247';
-DELETE FROM `worldstate_templates` WHERE `map`='489' AND `zone`='3277' AND `field`='4248';
-INSERT INTO `worldstate_templates` (`map`, `zone`, `field`, `value`) VALUES 
-('489', '3277', '4247', '1'),
-('489', '3277', '4248', '25'); 
-UPDATE `world_db_version` SET `LastUpdate`='2013-10-28_07_54_warsong_gulch' WHERE `LastUpdate` = '2013-10-28_16_16_creature_power_type';
--- sql/world_updates/2013-10-28_16-16_creature_power_type.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `creature_proto` ADD COLUMN `power_type` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `maxhealth`;
-ALTER TABLE `creature_proto` CHANGE COLUMN `mana` `minpower` INT(30) UNSIGNED NOT NULL DEFAULT '0' AFTER `power_type`;
-ALTER TABLE `creature_proto` ADD COLUMN `maxpower` INT(30) UNSIGNED NOT NULL DEFAULT '0' AFTER `minpower`;
-UPDATE `world_db_version` SET `LastUpdate`='2013-10-28_16-16_creature_power_type' WHERE  `LastUpdate`='2012-08-14_21-25_worldmap_info';
