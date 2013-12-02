00277e67f0adf1344fe24147a877d039b21a03a0
armm77 <amr.morales@gmail.com>
2013-12-02 00:47:40 -0430

    * Fix

--- sql/world_updates/2013-11-24_01_creature_spawns.sql
+++ sql/world_updates/2013-11-24_01_creature_spawns.sql
@@ -2,7 +2,6 @@ ALTER TABLE `creature_spawns`
 	COMMENT='Creature Spawn System',
 	CHANGE COLUMN `id` `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Global Unique Identifier' FIRST,
 	CHANGE COLUMN `entry` `entry` MEDIUMINT(10) UNSIGNED NOT NULL COMMENT 'Creature Identifier' AFTER `id`,
-	CHANGE COLUMN `map` `map` SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Map Identifier' AFTER `entry`
 	CHANGE COLUMN `map` `map` SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Map Identifier' AFTER `entry`,
 	CHANGE COLUMN `position_x` `position_x` FLOAT(8,2) NOT NULL DEFAULT '0' AFTER `map`,
 	CHANGE COLUMN `position_y` `position_y` FLOAT(8,2) NOT NULL DEFAULT '0' AFTER `position_x`,
