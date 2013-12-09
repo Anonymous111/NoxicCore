500a7e0a0cb38374391ffcf06d8430103633296d
Noxicfuse <zdroid9770@yahoo.com>
2013-12-08 18:21:09 -0600

    Scripted powerconv

--- src/scripts/src/GossipScripts/Gossip_AzuremystIsle.cpp
+++ src/scripts/src/GossipScripts/Gossip_AzuremystIsle.cpp
@@ -48,9 +48,62 @@ public:
 	}
 };
 
+class DraeneiFishingNet : public GossipScript
+{
+	public:
+		void GossipHello(Object* pObject, Player* pPlayer)
+		{
+			if(pObject == NULL || !pObject->IsItem() || pPlayer == NULL)
+				return;
+
+			QuestLogEntry* QuestEntry = pPlayer->GetQuestLogForEntry(9452);
+			if(QuestEntry == NULL)
+				return;
+
+#ifndef BLIZZLIKE
+			//if ( QuestEntry->GetMobCount( 0 ) >= QuestEntry->GetQuest()->required_mobcount[ 0 ] )
+			//	return;
+#endif
+			if(pPlayer->GetMapMgr() == NULL)
+				return;
+
+			// Meh, double object looking - we should find a way to remove this
+			GameObject* School = pPlayer->GetMapMgr()->GetInterface()->GetGameObjectNearestCoords(pPlayer->GetPositionX(), pPlayer->GetPositionY(), pPlayer->GetPositionZ(), 181616);
+			if(School == NULL || pPlayer->CalcDistance(School) > 5.0f)
+				return;
+
+#ifdef BLIZZLIKE
+			sEventMgr.AddEvent(School, &GameObject::Despawn, static_cast< uint32 >(20000), EVENT_GAMEOBJECT_ITEM_SPAWN, 1000, 1, 0);
+#else
+			School->Despawn(20000, 0);
+#endif
+			pPlayer->CastSpell(pPlayer, dbcSpell.LookupEntry(TO_ITEM(pObject)->GetProto()->Spells[ 0 ].Id), false);
+			uint32 Chance = RandomUInt(10);
+			if(Chance <= 3)
+			{
+				Creature* NewCreature = sEAS.SpawnCreature(pPlayer, 17102, pPlayer->GetPositionX(), pPlayer->GetPositionY(), pPlayer->GetPositionZ(), pPlayer->GetOrientation(), 180000);
+				if(NewCreature != NULL)
+				{
+					NewCreature->GetAIInterface()->StopMovement(500);
+					NewCreature->setAttackTimer(1000, false);
+					NewCreature->m_noRespawn = true;
+				};
+
+				return;
+			};
+
+			sEAS.AddItem(23614, pPlayer);
+			QuestEntry->SendUpdateAddKill(1);
+			QuestEntry->UpdatePlayerFields();
+			pPlayer->Gossip_Complete();
+		};
+
+};
+
 void SetupAzuremystIsleGossip(ScriptMgr* mgr)
 {
 	mgr->register_gossip_script(17440, new TheKesselRun_Gossip()); // High Chief Stillpine
 	mgr->register_gossip_script(17116, new TheKesselRun_Gossip()); // Exarch Menelaous
 	mgr->register_gossip_script(17240, new TheKesselRun_Gossip()); // Admiral Odesyus
+	//mgr->register_item_gossip_script( 23654, CREATE_GOSSIPSCRIPT( DraeneiFishingNet ) );
 }
