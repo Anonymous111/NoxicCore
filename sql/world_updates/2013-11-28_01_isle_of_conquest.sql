82af4448f839e266cee21f93d7997d71b68a717f
zdroid9770 <zdroid9770@yahoo.com>
2013-12-10 17:54:54 -0600

    Removed. Will add when I'm done.

--- sql/world_updates/2013-11-28_01_isle_of_conquest.sql
+++
@@ -1,8 +0,0 @@
-UPDATE `gameobject_names` SET `sound3`=0 WHERE  `entry`=191311;
-UPDATE `gameobject_names` SET `sound5`=0, `sound9`=22079 WHERE  `entry`=195494;
-UPDATE `gameobject_names` SET `sound5`=0, `sound9`=22083 WHERE  `entry`=195494;
-UPDATE `gameobject_names` SET `sound5`=0, `sound9`=22081 WHERE  `entry`=195496;
-UPDATE `gameobject_names` SET `sound5`=0, `sound9`=22078 WHERE  `entry`=195698;
-UPDATE `gameobject_names` SET `sound5`=0, `sound9`=22082 WHERE  `entry`=195699;
-UPDATE `gameobject_names` SET `sound5`=0, `sound9`=22080 WHERE  `entry`=195700;
-UPDATE `world_db_version` SET `LastUpdate`='2013-11-28_01_isle_of_conquest' WHERE `LastUpdate` = '2013-11-24_01_creature_spawns';
